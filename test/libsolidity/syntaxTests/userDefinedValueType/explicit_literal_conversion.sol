f33a311efcd62310512d2e99ad9bff336d80d569
chriseth <chris@ethereum.org>
2021-09-02 14:40:56 +0200

    Update tests.

--- test/libsolidity/syntaxTests/userDefinedValueType/explicit_literal_conversion.sol
+++ test/libsolidity/syntaxTests/userDefinedValueType/explicit_literal_conversion.sol
@@ -9,5 +9,5 @@ function f() pure {
 // ----
 // DeclarationError 2333: (136-145): Identifier already declared.
 // TypeError 9640: (89-101): Explicit type conversion not allowed from "int_const 5" to "user defined type MyAddress".
-// TypeError 9640: (119-130): Explicit type conversion not allowed from "int_const -5" to "user defined type MyUInt8". Cannot implicitly convert signed literal to unsigned type.
-// TypeError 9640: (148-160): Explicit type conversion not allowed from "int_const 500" to "user defined type MyUInt8". Literal is too large to fit in uint8.
+// TypeError 9640: (119-130): Explicit type conversion not allowed from "int_const -5" to "user defined type MyUInt8".
+// TypeError 9640: (148-160): Explicit type conversion not allowed from "int_const 500" to "user defined type MyUInt8".
